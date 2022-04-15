dc1654e7ce59e7028908f7c5fd387bc575326f94
Rafael Ventura <rafinskipg@gmail.com>
2022-04-16 00:04:53 +0200

    more

---
+++ scripts/resize.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const images = require('images');
+
+const { baseImages } = require("./baseImages");
+
+
+function resize(base, path) {
+    const image = images(path).size(500);
+  // const image = images(footy.parts.kit).size(size);
+
+ 
+
+  // Save image
+  const imagePath = `./output/resized/${base}/${path.split('/').pop()}.png`;
+  image.save(imagePath, {
+    quality: 100,
+  });
+}
+function execute() {
+    baseImages.base.forEach(b => {
+        resize('ballot-base', b)
+    })
+
+    baseImages.sigil.forEach(b => {
+        resize('ballot-sigil', b)
+    })
+
+    baseImages.signature.forEach(b => {
+        resize('ballot-signature', b)
+    })
+}
+
+execute()
